f2a15f6461427ddeb18e6b8153986958a0c870b1
TajiriSeiki <seiki.1226@gmail.com>
2025-02-09 21:35:20 +0900

    5まで回答

--- 003.go
+++ 003.go
@@ -6,7 +6,7 @@ func main() {
     var num int
 
 	fmt.Print("整数を入力してください: ")
-	_, err := fmt.Scan(&num)
+	_, err := fmt.Scanln(&num)
     if err != nil {
 		fmt.Println("エラーが発生しました:", err)
 		return
---
+++ 004.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+    var num int
+
+	fmt.Print("整数を入力してください: ")
+	_, err := fmt.Scanln(&num)
+    if err != nil {
+		fmt.Println("エラーが発生しました:", err)
+		return
+	}
+
+	fmt.Println("answer = ", num * 3)
+}
