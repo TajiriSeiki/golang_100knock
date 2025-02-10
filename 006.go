13a7444cebd2beeb2dffee6ec875ca29a7fff38c
TajiriSeiki <seiki.1226@gmail.com>
2025-02-10 09:34:23 +0900

    6回答

---
+++ 006.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+    var num int
+
+	fmt.Print("整数を入力してください: ")
+	_, err := fmt.Scanln(&num)
+    if err != nil {
+		fmt.Println("エラーが発生しました:", err)
+		return
+	}
+
+	if (num != 0) {
+		fmt.Println("not zero")
+	} else {
+		fmt.Println("zero")
+	}
+}
